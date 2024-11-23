755e702d065557d96ca31ec5f21bea0a7ed1e2ee
Goes Dev <jcgoes.dev@gmail.com>
2024-11-23 12:35:34 -0300

    minor changes + task management

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ task = {
     "Minutes": ""
 }
 
+active_task = []
+
 
 def update_counter(counter_type: str, minutes: int):
     """
@@ -88,6 +90,16 @@ def counter(counter_type: str, is_a_task: bool):
     os.system('cls')
     if is_a_task:
         start_counter_time = start_counter()
+        print("Select a Task")
+        count = 0
+        for t in active_task:
+            print(f"[{count}] {t[0]}")
+            count += 1
+        print("-- ", end="")
+        task_choosen = int(input())
+        task["Name"] = active_task[task_choosen][0]
+        task["Category"] = active_task[task_choosen][1]
+        os.system('cls')
         print(f"Task: {task['Name']} | Category: {task['Category']}")
     else:
         print("Resting Time")
@@ -112,12 +124,19 @@ def create_new_task():
     os.system('cls')
     print("Create a new Task")
     task["Name"] = input("Name: ").title()
-    category = input("[1] New Category [2] Existing Category ")
+    category = input("[1] New Category [2] Existing Category \n-- ")
     if category == "1":
         task["Category"] = input("Category: ").title()
     else:
         # Caso seja uma Categoria existente, chama a função a seguir
         select_existing_category()
+
+    cycles = input("N° of Cycles: ")
+
+    existing_task = [task["Name"], task["Category"], f"{cycles}/{cycles}"]
+    active_task.append(existing_task)
+
+    app_menu()
 
 
 def create_new_category():
@@ -157,11 +176,13 @@ def app_menu():
     os.system('cls')
     print("Hey Buddy!")
     print("[1] Pomodoro Counter | [2] Short Rest | [3] Long Rest")
-    print("[4] New Category")
+    print("[4] New Category     | [5] New Task\n")
+    print("~~ Active Tasks ~~")
+    for task in active_task:
+        print(f"{task[0]} | {task[1]} | {task[2]}")
     counter_type_choice = input("-- ")
     is_a_task = True
     if counter_type_choice == "1":
-        create_new_task()
         counter("Pomodoro", is_a_task)
     elif counter_type_choice == "2":
         is_a_task = False
@@ -171,6 +192,8 @@ def app_menu():
         counter("Long_Rest", is_a_task)
     elif counter_type_choice == "4":
         create_new_category()
+    elif counter_type_choice == "5":
+        create_new_task()
     else:
         print("Invalid Option")
 
